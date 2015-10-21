1c5d0fa0041730aab1ad6cff6253909c6a0fc759
djquirke <daniel_quirke@hotmail.co.uk>
2015-10-21 23:30:35 +0100

    working on gameover rules
    tile counter/code refactor/full grid game terminator

--- Reversi/Assets/GridManager.cs
+++ Reversi/Assets/GridManager.cs
@@ -2,20 +2,48 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public struct Tile
+enum Team
 {
-    public bool team;
-    public bool occupied;
+    BLACK,
+    WHITE,
+    NONE
+}
+
+struct Tile
+{
+    Team team;
+    bool occupied;
 
     //public Tile()
     //{
     //   team = false;
     //   occupied = false;
     //}
-    public Tile(bool team)
+
+    public Tile(Team team, bool occupied)
     {
         this.team = team;
-        occupied = true;
+        this.occupied = occupied;
+    }
+
+    public Team Team()
+    {
+        return team;
+    }
+
+    public void Team(Team team)
+    {
+        this.team = team;
+    }
+
+    public bool Occupied()
+    {
+        return occupied;
+    }
+    
+    public void Occupied(bool occupied)
+    {
+        this.occupied = occupied;
     }
 }
 
@@ -30,9 +58,9 @@ public class GridManager : MonoBehaviour
     public GameObject tileHighlight;
     public GameObject counterInst;
 
-    public int length = 8, width = 8;
-    Tile white = new Tile(false);
-    Tile black = new Tile(true);
+    public int width = 8, height = 8, blackCount = 0, whiteCount = 0;
+    Tile white = new Tile(Team.WHITE, true);
+    Tile black = new Tile(Team.BLACK, true);
 
     // Use this for initialization
     void Start()
@@ -45,19 +73,23 @@ public class GridManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (CheckEndGame())
+        {
+            //display win/lose screen
+        }
 
+        Debug.Log("Black tiles: " + blackCount + ", white tiles " + whiteCount + ".");
+        UpdateBoard();
     }
 
     void CreateBoard()
     {
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < height; i++)
         {
             List<Tile> tempList = new List<Tile>();
             for (int j = 0; j < width; j++)
             {
-                Tile tempTile = new Tile();
-                tempTile.team = false;
-                tempTile.occupied = false;
+                Tile tempTile = new Tile(Team.NONE, false);
                 tempList.Add(tempTile);
             }
             board.Add(tempList);
@@ -68,12 +100,14 @@ public class GridManager : MonoBehaviour
     {
         int wsmaller = (width / 2) - 1;
         int wlarger = (width / 2);
-        int lsmaller = (length / 2) - 1;
-        int llarger = (length / 2);
+        int lsmaller = (height / 2) - 1;
+        int llarger = (height / 2);
         board[wsmaller][lsmaller] = white;
         board[wlarger][llarger] = white;
         board[wsmaller][llarger] = black;
         board[wlarger][lsmaller] = black;
+        whiteCount += 2;
+        blackCount += 2;
     }
 
     void UpdateBoard()
@@ -83,20 +117,20 @@ public class GridManager : MonoBehaviour
             GameObject.Destroy(counter);
         }
 
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                if (board[i][j].occupied == true)
+                if (board[i][j].Occupied() == true)
                 {
                     //Set tile location
-                    float locX = i - (length - 1) / 2.0f;
+                    float locX = i - (height - 1) / 2.0f;
                     float locY = j - (width - 1) / 2.0f;
                     //Scale up location
                     locX *= 10.0f;
                     locY *= 10.0f;
                     GameObject counter = (GameObject)Instantiate(counterInst, new Vector3(locX, 1.5f, locY), Quaternion.Euler(Vector3.zero));
-                    if (board[i][j].team)
+                    if (board[i][j].Team() == Team.BLACK)
                     {
                         counter.transform.rotation = Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f));
                     }
@@ -105,11 +139,11 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    bool IsValidMove(int x, int y, bool team)
+    bool IsValidMove(int x, int y, Team team)
     {
-        if (x >= width || y >= length || x < 0 || y < 0)
+        if (x >= width || y >= height || x < 0 || y < 0)
             return false;
-        if (board[x][y].occupied == true)
+        if (board[x][y].Occupied() == true)
             return false;
         bool enemyFound = false;
         for (int i = -1; i <= 1; i++)
@@ -120,7 +154,7 @@ public class GridManager : MonoBehaviour
                 {
                     continue;
                 }
-                if (board[x + i][y + j].occupied == true && board[x + i][y + j].team != team)
+                if (board[x + i][y + j].Occupied() == true && OpposingCounter(board[x + i][y + j].Team(), team))
                 {
                     enemyFound = true;
                 }
@@ -130,6 +164,15 @@ public class GridManager : MonoBehaviour
             return false;
 
         return true;
+    }
+
+    bool OpposingCounter(Team first, Team second)
+    {
+        if ((first == Team.WHITE && second == Team.BLACK) || (first == Team.BLACK && second == Team.WHITE))
+        {
+            return true;
+        }
+        return false;
     }
 
     bool MakeMove(int x, int y, bool team)
@@ -143,10 +186,30 @@ public class GridManager : MonoBehaviour
 
     }
 
-    bool CheckVictory(bool team)
+    //checks for game terminating conditions
+    bool CheckEndGame()
     {
-        return false;
+        bool ret = false;
+
+        //check if any moves available for current players go
+        //if moves are available - ret = false
+        //no moves - ret = true
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                //if not a space is not occupied then board is not full
+                if (board[i][j].Occupied() == false)
+                    ret = false;
+            }
+        }
+
+        return ret;
     }
 
     bool EnemyInDirection()
+    {
+        return false;
+    }
 }
